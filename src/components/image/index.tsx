ea3893759b3dca967d8ce867959132daa15f1fda
Azrilardian <irfan05ariell@gmail.com>
2025-03-23 05:22:39 +0800

    refactor(image): replace custom image component with Next.js Image for improved performance and features

--- src/components/image/index.tsx
+++ src/components/image/index.tsx
@@ -1,17 +1,23 @@
 import React, { FC, memo } from 'react'
 
-import { ImageProps } from '@heroui/react'
-
-import { defaultImage } from 'assets/images'
+import NextImage, { ImageProps } from 'next/image'
 
 const Image: FC<ImageProps> = (props) => {
-  const {
-    alt = 'An awesome image',
-    fallbackSrc = defaultImage,
-    ...rest
-  } = props
+  const { alt = 'An awesome image', ...rest } = props
 
-  return <Image alt={alt} fallbackSrc={fallbackSrc} {...rest} />
+  return (
+    <NextImage
+      alt={alt}
+      loading='lazy'
+      decoding='async'
+      quality={65}
+      fill
+      style={{ objectFit: 'cover' }}
+      placeholder='blur'
+      blurDataURL={`/_next/image?url=${rest.src}&w=16&q=1`}
+      {...rest}
+    />
+  )
 }
 
 export default memo(Image)
