6c198ef8245793829c2deca6fc27f57e43d675ba
Azrilardian <irfan05ariell@gmail.com>
2025-04-04 01:01:38 +0800

    feat(providers): integrate Next.js router into HeroUIProvider for navigation support

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,13 +1,18 @@
+'use client'
+
 import { FC } from 'react'
 
 import { HeroUIProvider } from '@heroui/react'
+import { useRouter } from 'next/navigation'
 
 import { ProvidersProps } from './types'
 
 const Providers: FC<ProvidersProps> = (props) => {
   const { children } = props
 
-  return <HeroUIProvider>{children}</HeroUIProvider>
+  const router = useRouter()
+
+  return <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>
 }
 
 export default Providers
