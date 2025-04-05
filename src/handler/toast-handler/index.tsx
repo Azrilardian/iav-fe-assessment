29bb9468b2dc59bde704a73fccf54f3ce7585488
Azrilardian <irfan05ariell@gmail.com>
2025-04-05 11:59:05 +0800

    fix(toast-handler): add expand prop to Toaster component for improved visibility

--- src/handler/toast-handler/index.tsx
+++ src/handler/toast-handler/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Toaster } from 'sonner'
 
 const ToastHandler = () => {
-  return <Toaster richColors position='top-right' />
+  return <Toaster richColors position='top-right' expand />
 }
 
 export default ToastHandler
