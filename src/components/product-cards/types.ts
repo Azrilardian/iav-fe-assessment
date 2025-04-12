580a16aad265f484e488af20a38dd048c13f20d1
Azrilardian05 <azril@itsavirus.com>
2025-04-12 12:14:33 +0800

    chore(cleanup): remove unused authentication and UI components from the application

--- src/components/product-cards/types.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from 'react'
-
-import { ProductCardProps } from '../product-card/types'
-
-export type ProductCardsProps = ComponentProps<'div'> & {
-  products: ProductCardProps[]
-}
--- src/services/swr/middleware/collection-adaptor.types.ts
+++
@@ -1,7 +0,0 @@
-export type Collection<K extends string, T extends object> = {
-  [key in K]: T[]
-} & {
-  totalPages: number
-  totalItems: number
-  currentPage: number
-}
