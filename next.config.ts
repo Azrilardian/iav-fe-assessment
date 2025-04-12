498e450c063f70bafed21bc670a754bd73828a57
Azrilardian05 <azril@itsavirus.com>
2025-04-12 17:43:44 +0800

    feat(config): update Next.js config with environment variables, add image remote patterns, and implement redirects; enhance Tailwind CSS configuration with centered container and padding settings

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,26 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     cssChunking: true,
-    useLightningcss: true,
     optimizePackageImports: ['@heroui/react']
   },
   env: {
-    API_BASE_URL: '',
-    SENTRY_DSN: '',
-    ENVIRONMENT: ''
+    API_BASE_URL: process.env.API_BASE_URL,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    ENVIRONMENT: process.env.ENVIRONMENT
   },
-  trailingSlash: true
+  trailingSlash: true,
+  images: {
+    remotePatterns: [{ hostname: 'cdn.dummyjson.com' }]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/products',
+        permanent: true
+      }
+    ]
+  }
 }
 
 export default nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,6 +35,14 @@ export default {
     },
     screens: {
       ...screens
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.5rem',
+        lg: '2rem'
+      }
     }
   },
   plugins: [
